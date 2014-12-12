830431f8f826b50ecd3b88973ec38fa4d00a900a
Jason Hildebrand <jason@peaceworks.ca>
2014-12-12 16:38:03 -0600

    Clean up default taxes implementation and added comments.

--- plugins/sfEcommercePlugin/lib/sfEcommerceTaxes.class.php
+++ plugins/sfEcommercePlugin/lib/sfEcommerceTaxes.class.php
@@ -5,43 +5,49 @@
 
 class sfEcommerceTaxes 
 {
-  public static $hst_provinces = array(
-    'New Brunswick' => '13',
-    'Newfoundland and Labrador' => '13',
-    'Nova Scotia' => '15',
-    'Ontario' => '13',
-    'Prince Edward Island' => '14',
-  );
 
   public static function determine_taxes($sale, $saleResource, $repo)
   {
     $taxes = array();
 
-    if ($repo->slug == 'jasonarc') {
-      $repo_prov = 'Manitoba';
-    } else if ($repo->slug == 'jasonarc-2') {
-      $repo_prov = 'Ontario';
-    }
+    /* Applicable taxes could depend on:
+      * which repository is making the sale
+      * the country and/or region of the customer
+
+      Check legal requirements with your bookkeeper or tax consultant.
+
+      Then implement appropriate logic for your situation.
+      Note that the sample code below is INCOMPLETE.
+
+      Append applicable taxes and rates to the $taxes array
+      and return it.  
+    */
+
+    /*
+    $hst_provinces = array(
+      'New Brunswick' => '13',
+      'Newfoundland and Labrador' => '13',
+      'Nova Scotia' => '15',
+      'Ontario' => '13',
+      'Prince Edward Island' => '14',
+    );
+
+    $repository = $repo->slug;
 
     $supply_country = $sale->country;
     if ($supply_country != 'CA') {
+      // no applicable taxes if customer is outside Canada
       return $taxes;
     }
     $supply_province = $sale->province;
 
-    if ($repo_prov == 'Ontario') {
-      if (array_key_exists($supply_province, self::$hst_provinces)) {
-        $hstprovs = sfEcommerceTaxes::$hst_provinces;
-        $taxes['HST'] = $hstprovs[$supply_province];
+    if ($repository == 'test123') {
+      if (array_key_exists($supply_province, $hst_provinces)) {
+        $taxes['HST'] = $hst_provinces[$supply_province];
       } else {
         $taxes['GST'] = '5';
       }
-    } elseif ($repo_prov == 'Manitoba') {
-      $taxes['GST'] = '5';
-      if ($supply_province == 'Manitoba') {
-        $taxes['PST'] = '8';
-      }
-    }
+    } */
     return $taxes;
   }
 }
